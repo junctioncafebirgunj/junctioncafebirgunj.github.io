58e72474d64ffc9de5532e4972819e6ef90a47e6
junctioncafebirgunj <rajababu19086@gmail.com>
2026-07-28 12:49:09 +0545

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,44 @@
-console.log("Menu Loaded!");
+const menu = [
+  {
+    name: "Chicken Momo",
+    price: "Rs. 180",
+    category: "Momo"
+  },
+  {
+    name: "Veg Momo",
+    price: "Rs. 150",
+    category: "Momo"
+  },
+  {
+    name: "Chicken Chowmein",
+    price: "Rs. 220",
+    category: "Chowmein"
+  },
+  {
+    name: "Veg Chowmein",
+    price: "Rs. 180",
+    category: "Chowmein"
+  },
+  {
+    name: "Chicken Burger",
+    price: "Rs. 250",
+    category: "Burger"
+  },
+  {
+    name: "French Fries",
+    price: "Rs. 120",
+    category: "Snacks"
+  },
+  {
+    name: "Cold Coffee",
+    price: "Rs. 180",
+    category: "Drinks"
+  },
+  {
+    name: "Lemon Soda",
+    price: "Rs. 100",
+    category: "Drinks"
+  }
+];
+
+console.log(menu);
